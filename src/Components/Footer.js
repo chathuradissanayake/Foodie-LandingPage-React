6e255d43f6dc508879058adc357059eb10959fbb
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-07-22 17:52:37 +0530

    Resolve errors

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,46 +2,44 @@ import React from 'react';
 import { BsTwitter, BsYoutube } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 import { SiLinkedin } from 'react-icons/si';
+import Logo from "../Assets/Logo.svg";
 
 
 const Footer = () => {
-  return (
-    <div className='footer-wrpper'>
-        <div className='footer-section-one'>
-            <div className='footer-logo-container'>
-                <img src='Logo' alt=''/>
-            </div>
-            <div className='footer-icons'>
-                <BsTwitter/>
-                <FaFacebookF/>
-                <BsYoutube/>
-                <SiLinkedin/>
-            </div>
+    return (
+      <div className="footer-wrapper">
+        <div className="footer-section-one">
+          <div className="footer-logo-container">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="footer-icons">
+            <BsTwitter />
+            <SiLinkedin />
+            <BsYoutube />
+            <FaFacebookF />
+          </div>
         </div>
-        <div className='footer-section-two'>
-            <div className='footer-section-columns'>
-                <span>Quality</span>
-                <span>Help</span>
-                <span>Share</span>
-                <span>Careers</span>
-                <span>Testimonial</span>
-                <span>Work</span>
-            </div>
-            <div className='footer-section-columns'>
-                <span>+94773084850</span>
-                <span>chathura@foodie.com</span>
-                <span>helloworld@foodie.com</span>
-                <span>press@foodie.com</span>
-                <span>contact@foodie.com</span>
-            </div>
-            <div className='footer-section-columns'>
-                <span>Terms & Conditions</span>
-                <span>Privacy Policy</span>
-            </div>
+        <div className="footer-section-two">
+          <div className="footer-section-columns">
+            <span>Qualtiy</span>
+            <span>Help</span>
+            <span>Share</span>
+            <span>Carrers</span>
+            <span>Testimonials</span>
+            <span>Work</span>
+          </div>
+          <div className="footer-section-columns">
+            <span>+94773084850</span>
+            <span>chathura@food.com</span>
+            <span>press@food.com</span>
+            <span>contact@food.com</span>
+          </div>
+          <div className="footer-section-columns">
+            <span>Terms & Conditions</span>
+            <span>Privacy Policy</span>
+          </div>
         </div>
-
-    </div>
-  )
-}
-
+      </div>
+    );
+  };
 export default Footer
